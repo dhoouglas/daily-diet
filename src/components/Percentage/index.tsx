b215fe9b4561fcc5a2a75dec56e00e421550a8fa
dhoouglas <dhoouglas@hotmail.com>
2025-06-02 15:05:01 -0300

    feat: created component percentage and style Home

---
+++ src/components/Percentage/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Description, StatisticsIcon, Title } from "./styles";
+
+export function Percentage() {
+  return (
+    <Container>
+      <StatisticsIcon weight="bold" />
+
+      <Title>90,86%</Title>
+
+      <Description>das refeições dentro da dieta</Description>
+    </Container>
+  );
+}
